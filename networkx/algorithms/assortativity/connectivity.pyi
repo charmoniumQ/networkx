31a54dad7b510a0204017b02d861d5ee8520e731
Samuel Grayson <sam@samgrayson.me>
2022-07-20 22:31:57 -0500

    Ran stubgen

---
+++ networkx/algorithms/assortativity/connectivity.pyi
@@ -0,0 +1,3 @@
+from _typeshed import Incomplete
+
+def average_degree_connectivity(G, source: str = ..., target: str = ..., nodes: Incomplete | None = ..., weight: Incomplete | None = ...): ...
---
+++ networkx/algorithms/link_analysis/hits_alg.pyi
@@ -0,0 +1,3 @@
+from _typeshed import Incomplete
+
+def hits(G, max_iter: int = ..., tol: float = ..., nstart: Incomplete | None = ..., normalized: bool = ...): ...
