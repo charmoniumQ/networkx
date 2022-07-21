123f90a13bdf91eed363a7c26cf6b31652b32427
Samuel Grayson <sam@samgrayson.me>
2022-07-20 22:26:22 -0500

    Corrected type stubs

--- networkx/algorithms/flow/maxflow.pyi
+++ networkx/algorithms/flow/maxflow.pyi
@@ -1,7 +1,8 @@
-from .preflowpush import preflow_push
 from _typeshed import Incomplete
 
-default_flow_func = preflow_push
+from .flow_func import _FlowFunc
+
+default_flow_func: _FlowFunc = ...
 
 def maximum_flow(flowG, _s, _t, capacity: str = ..., flow_func: Incomplete | None = ..., **kwargs): ...
 def maximum_flow_value(flowG, _s, _t, capacity: str = ..., flow_func: Incomplete | None = ..., **kwargs): ...
